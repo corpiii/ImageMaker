59fc9a565cb3b68ba671f872c5f78aa214873e8e
jjpush <lj845854@gmail.com>
2023-07-29 22:04:09 +0900

    feat: Negative Prompt 뷰 생성 및 설정

--- ImageMaker/MainViewController.swift
+++ ImageMaker/MainViewController.swift
@@ -1,3 +1,5 @@
+//
+//  MainViewController.swift
 //  ImageMaker
 //
 //  Created by 이정민 on 2023/07/29.
@@ -77,6 +79,54 @@ class MainViewController: UIViewController {
         
         return textView
     }()
+    
+    // MARK: - Negative Prompt Stack
+    
+    private let negativePromptStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        
+        return stackView
+    }()
+    
+    private let negativePromptLabelStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        
+        return stackView
+    }()
+    
+    private let negativePromptLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Negative prompt"
+        label.textColor = Constant.textColor
+        label.font = .boldSystemFont(ofSize: 20)
+        
+        return label
+    }()
+    
+    private let negativePromptTextCountLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "0 / 256"
+        label.textColor = .lightGray
+        label.font = .preferredFont(forTextStyle: .caption2)
+        
+        return label
+    }()
+    
+    private let negativePromptTextView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.layer.cornerRadius = 8.0
+        textView.layer.borderWidth = 1.0
+        textView.layer.borderColor = UIColor.lightGray.cgColor
+        
+        return textView
+    }()
     // MARK: - View Life Cycle Method
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -120,5 +170,21 @@ private extension MainViewController {
         promptLabelStackView.addArrangedSubview(UIView())
         promptLabelStackView.addArrangedSubview(promptTextCountLabel)
         
+    }
+    
+    // MARK: - Negative Prompt setting
+    func configNegativePromptStackView() {
+        negativePromptStackView.addArrangedSubview(negativePromptLabelStackView)
+        negativePromptStackView.addArrangedSubview(negativePromptTextView)
+        
+        negativePromptStackView.spacing = 10
+        
+        negativePromptTextView.snp.makeConstraints { make in
+            make.height.equalTo(100)
+        }
+        
+        negativePromptLabelStackView.addArrangedSubview(negativePromptLabel)
+        negativePromptLabelStackView.addArrangedSubview(UIView())
+        negativePromptLabelStackView.addArrangedSubview(negativePromptTextCountLabel)
     }
 }
